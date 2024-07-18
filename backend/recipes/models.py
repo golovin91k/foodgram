3231407ec5a7c194471db9c87b0e3cf4d3941ee7
Kirill Golovin <golovin91k@gmail.com>
2024-07-19 01:30:04 +0300

    before correct utils

--- backend/recipes/models.py
+++ backend/recipes/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
 
 from .constans import (
     TAG_NAME_MAX_LENGTH, TAG_SLUG_MAX_LENGTH, INGREDIENT_NAME_MAX_LENGTH,
@@ -135,6 +136,10 @@ class Subscription(models.Model):
             models.UniqueConstraint(fields=['author', 'subscriber'],
                                     name='unique_favorite')
         ]
+
+    def clean(self):
+        if self.author == self.subscriber:
+            raise ValidationError('Нельзя подписаться на самого себя.')
 
     def __str__(self):
         return f'{self.author} - {self.subscriber}'
